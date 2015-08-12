d3a7dd048ff7b91b7433847d7c6863b9c5852032
Tony Zou <tony@tonyzou.com>
2015-08-13 09:38:10 +1000

    WIP

--- integration_test.go
+++ integration_test.go
@@ -13,50 +13,51 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/service/kinesis"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/pborman/uuid"
 	"github.com/remind101/kinesumer/checkpointers/redis"
 	"github.com/remind101/kinesumer/provisioners/redis"
 	"github.com/remind101/kinesumer/redispool"
 )
 
-type NSync struct {
-	In  chan struct{}
-	Out chan struct{}
-}
-
-func NewNSync(n int) NSync {
-	sync := NSync{
-		In:  make(chan struct{}),
-		Out: make(chan struct{}),
-	}
-	go func() {
-		for {
-			for i := 0; i < n; i++ {
-				_, ok := <-sync.In
-				if !ok {
-					if i > 0 {
-						panic("NSync stream closed")
-					}
-					close(sync.Out)
-					return
-				}
-			}
-			for i := 0; i < n; i++ {
-				sync.Out <- struct{}{}
-			}
+func consecAll(n string, char rune) bool {
+	for _, c := range n {
+		if c != char {
+			return false
 		}
-	}()
-	return sync
+	}
+	return true
 }
 
-func (n NSync) Sync() {
-	n.In <- struct{}{}
-	<-n.Out
+func consec(n, sn string) bool {
+	if len(n) == 0 || len(sn) == 0 {
+		return false
+	} else if n[0] == sn[0] {
+		return consec(n[1:], sn[1:])
+	} else if n[0]+1 == sn[0] {
+		return consecAll(n[1:], '9') && consecAll(sn[1:], '0')
+	} else {
+		return false
+	}
+}
+
+func TestConsec(t *testing.T) {
+	if consec("123", "1234") {
+		t.Fail()
+	}
+
+	if !consec("123", "124") {
+		t.Fail()
+	}
+
+	if !consec("1233999", "1234000") {
+		t.Fail()
+	}
 }
 
 func TestIntegration(t *testing.T) {
 	rand.Seed(94133)
 
-	t.Logf("Creating Kinesis")
+	fmt.Println("Creating Kinesis")
 	kin := kinesis.New(&aws.Config{
 		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"),
 			os.Getenv("AWS_SECRET_ACCESS_KEY"), ""),
@@ -72,7 +73,7 @@ func TestIntegration(t *testing.T) {
 	redisPrefix := os.Getenv("REDIS_PREFIX")
 
 	cpOpt := redischeckpointer.Options{
-		SavePeriod:  time.Second,
+		SavePeriod:  3 * time.Second,
 		RedisPool:   redisPool,
 		RedisPrefix: redisPrefix,
 	}
@@ -82,10 +83,11 @@ func TestIntegration(t *testing.T) {
 	}
 
 	provOpt := redisprovisioner.Options{
-		TTL:         time.Second,
+		TTL:         3 * time.Second,
 		RedisPool:   redisPool,
 		RedisPrefix: redisPrefix,
 	}
+	provOpt2 := provOpt
 	prov, err := redisprovisioner.New(&provOpt)
 	if err != nil {
 		panic(err)
@@ -94,13 +96,13 @@ func TestIntegration(t *testing.T) {
 	kinOpt := KinesumerOptions{
 		ListStreamsLimit:    25,
 		DescribeStreamLimit: 25,
-		GetRecordsLimit:     1,
+		GetRecordsLimit:     10,
 		PollTime:            1,
 		MaxShardWorkers:     2,
 		Handlers:            DefaultHandlers{},
 	}
 
-	t.Logf("Creating Kinesumer")
+	fmt.Println("Creating Kinesumer")
 	k, err := NewKinesumer(kin, cp, prov, nil, stream, &kinOpt)
 	if err != nil {
 		panic(err)
@@ -112,7 +114,7 @@ func TestIntegration(t *testing.T) {
 	}
 
 	if exists {
-		t.Logf("Deleting stream")
+		fmt.Println("Deleting stream")
 		_, err := kin.DeleteStream(&kinesis.DeleteStreamInput{
 			StreamName: aws.String(stream),
 		})
@@ -132,7 +134,7 @@ func TestIntegration(t *testing.T) {
 	}
 cont1:
 
-	t.Logf("Creating stream")
+	fmt.Println("Creating stream")
 	_, err = kin.CreateStream(&kinesis.CreateStreamInput{
 		ShardCount: aws.Int64(3),
 		StreamName: aws.String(stream),
@@ -157,6 +159,8 @@ cont2:
 		panic(err)
 	}
 
+	time.Sleep(5 * time.Second)
+
 	if len(workers) != 2 {
 		panic(fmt.Sprintf("Expected 2 workers to be started by k. Workers: %v",
 			workers,
@@ -168,7 +172,7 @@ cont2:
 		panic(err)
 	}
 
-	prov2, err := redisprovisioner.New(&provOpt)
+	prov2, err := redisprovisioner.New(&provOpt2)
 	if err != nil {
 		panic(err)
 	}
@@ -186,21 +190,151 @@ cont2:
 	}
 
 	if len(workers2) != 1 {
-		panic("Expected 1 worker to be started by k2")
+		panic(fmt.Sprintf("Expected 1 worker to be started by k2. Workers: %v", workers2))
 	}
 
-	records := make([]*PutRecordsRequestEntry, 100)
+	records := make([]*kinesis.PutRecordsRequestEntry, 100)
+	values := make(map[string]bool)
 	for i := 0; i < 100; i++ {
+		item := uuid.New()
+		values[item] = true
 		records[i] = &kinesis.PutRecordsRequestEntry{
-			Data:         []byte(""),
-			PartitionKey: aws.String("aeou"),
+			Data:         []byte(item),
+			PartitionKey: aws.String(item),
 		}
 	}
 
-	kin.PutRecords(&kinesis.PutRecordsInput{
+	res, err := kin.PutRecords(&kinesis.PutRecordsInput{
 		Records:    records,
 		StreamName: aws.String(stream),
 	})
+	if err != nil {
+		panic(err)
+	}
+	if aws.Int64Value(res.FailedRecordCount) != 0 {
+		panic(fmt.Sprintf("Failed to put records: %v", res.Records))
+	}
 
-	t.FailNow()
+	timeout := time.NewTimer(time.Minute)
+	for count := 0; count < 100; count++ {
+		select {
+		case rec := <-k.Records():
+			if !values[string(rec.Data())] {
+				panic(fmt.Sprintf("Received unexpected record %v", rec))
+			} else {
+				delete(values, string(rec.Data()))
+			}
+		case rec := <-k2.Records():
+			if !values[string(rec.Data())] {
+				panic(fmt.Sprintf("Received unexpected record %v", rec))
+			} else {
+				delete(values, string(rec.Data()))
+			}
+		case <-timeout.C:
+			panic("Timed out fetching records")
+		}
+	}
+
+	if len(values) > 0 {
+		panic("Did not receive all expected records")
+	}
+
+	fmt.Println("Basic functionality works")
+
+	producerStopC := make(chan struct{}, 1)
+	consumerStopC := make(chan struct{}, 1)
+	stopped := make(chan struct{}, 1)
+	nProduced := 0
+	nConsumed := 0
+
+	go func() {
+		for {
+			select {
+			case <-producerStopC:
+				consumerStopC <- struct{}{}
+				return
+			default:
+				time.Sleep(time.Second)
+				res, err := kin.PutRecords(&kinesis.PutRecordsInput{
+					Records:    records,
+					StreamName: aws.String(stream),
+				})
+				if err != nil {
+					panic(err)
+				}
+				if aws.Int64Value(res.FailedRecordCount) != 0 {
+					panic(fmt.Sprintf("Failed to put records: %v", res.Records))
+				}
+				nProduced += len(records)
+			}
+		}
+	}()
+
+	go func() {
+		run := true
+		for run || nConsumed < nProduced {
+			select {
+			case <-k.Records():
+				nConsumed++
+			case <-k2.Records():
+				nConsumed++
+			case <-consumerStopC:
+				run = false
+			}
+		}
+		stopped <- struct{}{}
+	}()
+
+	shards, err := k.GetShards()
+	if err != nil {
+		panic(err)
+	}
+	if len(shards) != 3 {
+		panic("Expected 3 shards")
+	}
+
+	type pair struct {
+		begin, end int
+	}
+	pairs := []pair{
+		{0, 1},
+		{0, 2},
+		{1, 2},
+		{2, 1},
+	}
+	for _, pair := range pairs {
+		if consec(*shards[pair.begin].SequenceNumberRange.StartingSequenceNumber,
+			*shards[pair.end].SequenceNumberRange.EndingSequenceNumber) {
+			_, err := kin.MergeShards(&kinesis.MergeShardsInput{
+				ShardToMerge:         shards[pair.begin].ShardID,
+				AdjacentShardToMerge: shards[pair.end].ShardID,
+				StreamName:           aws.String(stream),
+			})
+			if err != nil {
+				panic(err)
+			}
+			break
+		}
+	}
+
+	timeout.Reset(time.Minute)
+	select {
+	case <-timeout.C:
+		panic("Shard worker did not stop after shard closed")
+	case <-k.stopped:
+		k.stopped <- Unit{}
+	case <-k2.stopped:
+		k2.stopped <- Unit{}
+	}
+
+	producerStopC <- struct{}{}
+	timeout.Reset(time.Minute)
+	select {
+	case <-timeout.C:
+		panic("Messages dropped")
+	case <-stopped:
+	}
+
+	k.End()
+	k2.End()
 }
